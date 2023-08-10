e71656611278fea0362b3608340c446f3a8c66d6
gingjin <gingjin@163.com>
2023-08-10 16:33:08 +0800

    update 'config.def.h': modify fonts

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static int restrict_return = 1;				/* -r option; if 1, disables shift-return and ctrl-return */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"LXGW WenKai Mono:style=Bold:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
